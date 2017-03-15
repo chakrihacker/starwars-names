c9941f1001e5eeef259c3074b9b37473eaff7593
chakri <chanty.chakravarthy@gmail.com>
2017-03-15 10:20:46 +0530

    chore(release): Add travis config, conventional commit and semantic release
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+let starWars = require('./index');
+
+describe('starwars-names', function () {
+  describe('all', function () {
+    it('should be an array of strings', function () {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function (item) {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('should contain `Luke Skywalker`', function () {
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
+  })
+  describe('random', function () {
+    it('should return a random item from the starWars.all', function () {
+      let randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    })
+  })
+});
